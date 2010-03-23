90e52c99285b3c53ef76fa187c28186b63d99855
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-03-23 15:53:10 +0100

    Added tests for error handling

--- spec/spec.simplifier.js
+++ spec/spec.simplifier.js
@@ -180,6 +180,95 @@ describe 'Simplifier'
       );        
     end    
   end
+  
+  /**
+    Error handling during execution
+  **/
+  describe 'Error handling during execution'
+    it 'Should correctly handle simple error returned from serial function'
+      // Keep track of running
+      var running = true;
+    
+      //
+      //  Execute all the functions and feed results into final method
+      //  
+      new simplifier.Simplifier().execute(
+        // Flows to execute
+        function(callback) { callback("Some error", null); }, 
+        function(err, requestDoc, callback) { callback(null, {doc:'userDoc'}); },
+        // All results coming back are arrays function1 [err, doc] function2 [err, doc1, doc2]
+        function(err, userDocResult) {  
+          err.should.not.be_null
+          err.should.eql "Some error"
+          // Signal test finished
+          running = false;
+        }
+      );       
+
+      var intervalId = setInterval(function() {
+        while(running) {}
+        clearInterval(intervalId);
+      }, 100);                   
+      // Trigger mock timer to start the execution
+      tick(100);                
+    end
+
+    it 'Should correctly handle exception being thrown from serial function'
+      // Keep track of running
+      var running = true;
+    
+      //
+      //  Execute all the functions and feed results into final method
+      //  
+      new simplifier.Simplifier().execute(
+        // Flows to execute
+        function(callback) { throw "Exception"; }, 
+        function(err, requestDoc, callback) { callback(null, {doc:'userDoc'}); },
+        // All results coming back are arrays function1 [err, doc] function2 [err, doc1, doc2]
+        function(err, userDocResult) {  
+          err.should.not.be_null
+          err.should.eql "Exception"
+          // Signal test finished
+          running = false;
+        }
+      );       
+
+      var intervalId = setInterval(function() {
+        while(running) {}
+        clearInterval(intervalId);
+      }, 100);             
+      // Trigger mock timer to start the execution
+      tick(100);                
+    end
+  end
+  
+  it 'Should correctly handle simple error returned from parallel execution'
+    // Keep track of running
+    var running = true;
+    // Define a parallel flow
+    var parallelFlow = new simplifier.ParallelFlow(
+      function(callback) { callback("Some error", {doc:'requestDoc'}); }, 
+      function(callback) { throw "Exception"; }
+    );
+  
+    //
+    //  Execute all the functions and feed results into final method
+    //  
+    new simplifier.Simplifier().execute(
+      parallelFlow,
+      // All results coming back are arrays function1 [err, doc] function2 [err, doc1, doc2]
+      function(requestDoc, userDoc) { 
+        requestDoc[0].should.not.be_null
+        requestDoc[0].should.eql "Some error"
+
+        userDoc[0].should.not.be_null
+        userDoc[0].should.eql "Exception"
+
+        // Signal test finished
+        running = false;
+      }
+    );        
+  end
 end
 
 
